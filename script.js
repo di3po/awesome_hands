efa497851b0c47d9018d7d14d5eca6db9b261e4c
di3po <bolatbekd1n@gmail.com>
2023-06-28 22:02:07 +0600

    changed onclick to onmouseover

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     let hand = document.getElementById('sender_hand')
-    hand.onclick = () => {
+    hand.onmouseover = () => {
         hand.style.transform = 'rotate(60deg)';
     }
-
-    document.getElementById('receiver').onclick = () => {
+    hand.onmouseout = () => {
         hand.style.transform = 'rotate(0deg)';
     }
+
+    /*
+        hand.onclick = () => {
+            hand.style.transform = 'rotate(60deg)';
+        }
+        
+        document.getElementById('receiver').onclick = () => {
+            hand.style.transform = 'rotate(0deg)';
+    }*/
 });
