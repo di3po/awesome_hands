b923ed0de1ed4f2fc81ed41f73006dabc680e183
Dinara <48620229+di3po@users.noreply.github.com>
2023-06-29 18:18:02 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(event)
 {
     let hand = document.getElementById('sender_hand')
-    hand.onmouseover = () => {
+    /*hand.onmouseover = () => {
         hand.style.transform = 'rotate(60deg)';
     }
     hand.onmouseout = () => {
         hand.style.transform = 'rotate(0deg)';
-    }
+    }*/
 
     /*hand.onclick = () => {
         hand.style.transform = 'rotate(60deg)';
